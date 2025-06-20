d93420b892d4596e3cace7ee808bdea668caea9b
nathaneast <ahrl_ahrl@naver.com>
2025-06-20 19:18:22 +1200

    feat: changed categories

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,7 @@ export const Header: React.FC<HeaderProps> = ({
             </div>
           </div>
 
-          <div className="flex items-center space-x-4">
+          {/* <div className="flex items-center space-x-4">
             <div className="relative">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
               <input
@@ -53,7 +53,7 @@ export const Header: React.FC<HeaderProps> = ({
                 className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent w-64"
               />
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </header>
